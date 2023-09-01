af5413019279d2534896d6e1924bb2f5763b29b1
natethor <natethor@users.noreply.github.com>
2023-08-31 21:23:01 -0400

    fix readme and plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
-  import "$lib/css/app.css";
-
   import Footer from "$lib/components/Footer.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
   import config from "$lib/content/data";
+  import "$lib/css/app.css";
+  import { initPlausible } from "$lib/utils/stats";
+  import { onMount } from "svelte";
 
   const keywords = config.keywords;
+
+  onMount(() => {
+    initPlausible();
+  });
 </script>
 
 <svelte:head>
